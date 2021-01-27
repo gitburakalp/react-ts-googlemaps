3be0a36951a4b8f63798daa19b8a4f1b42b8b32a
gitburakalp <burakalp17@gmail.com>
2021-01-27 15:21:30 +0300

    1 file modified

--- GoogleMaps/index.tsx
+++ GoogleMaps/index.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import fetch from "isomorphic-fetch";
 
 const API_KEY = "AIzaSyDF98ueUQD0nn_26-_RglOtLdkXjsKaEnA";
+const ADRESLER = [
+  "İSTANBUL / Beşiktaş",
+  "ANKARA / Merkez",
+  "İZMİR / Merkez",
+  "ANTALYA / Konyaaltı",
+  "LONDRA",
+  "NEW YORK"
+];
 
 const Test: React.FC = () => {
+  interface StateInterfaceItems {
+    id: number;
+    lat: number;
+    lng: number;
+  }
+
+  interface StateInterfaceItems extends Array<StateInterfaceItems> {}
+
+  useEffect(() => {
+    ADRESLER.map(adres => {
+      var url = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+${adres}&key=${API_KEY}`;
+
+      const getMarkers = async () => {
+        const response = await fetch(url);
+        if (response.status !== 400) {
+          const content = await response.json();
+
+          content.results.map(res => {
+            var loc = res.geometry.location;
+            console.log(loc.lat);
+          });
+        }
+      };
+
+      getMarkers();
+    });
+  });
+
   return <div>TEST</div>;
 };
 
